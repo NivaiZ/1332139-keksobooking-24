35ae33970ed6c9cccfb3485c00ddd04b9366c45b
ilya <nivaiz54@gmail.com>
2022-02-05 12:43:33 +0500

    Реализовал показ сообщения сети с библиотекой offline.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ import './popupuser.js';
 import './filter.js';
 import './backend.js';
 import './avatar.js';
+import './offline.js';
 disablePopupForm();
 disableMapFilters();
 activatePopupForm();
 activateMapFilters();
+
+const run = function(){
+  const req = new XMLHttpRequest();
+  req.timeout = 5000;
+  req.open('GET', 'http://localhost:8888/walter/0', true);
+  req.send();
+};
+
+setInterval(run, 3000);
